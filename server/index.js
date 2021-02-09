2f95d513d6cddac055d1560b4fd51e354a71121d
d.shelest <d.shelest@kt-team.de>
2021-02-09 13:15:29 +0300

    as

--- server/index.js
+++ server/index.js
@@ -9,4 +9,11 @@ app.use(express.json())
 app.use(cors())
 app.use('/api/posts', posts)
 
+if (process.env.NODE_ENV === 'production') {
+  //static folder
+  app.use(express.static(__dirname + '/public'))
+  //handle spa
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
+}
+
 app.listen(3000)
